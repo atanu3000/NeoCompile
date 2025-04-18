747f9007992409ff1d96ccbf0cd583b7e9c1e991
LeLoUcH-V-BrItAnIa <139791448+LeLoUcH-V-BrItAnIa@users.noreply.github.com>
2025-04-18 18:02:58 +0530

    Added routes using router module.. and some modifications

---
+++ server/routes/routes.js
@@ -0,0 +1,16 @@
+// Import Libraries
+const express = require("express");
+const router = express.Router();
+
+// Import Controllers
+const codeController = require("");
+const explanationController = require("");
+
+// Code Routes
+router.post("/runCode", codeController.run_code);
+router.post("/compileCode", codeController.compile_code);
+
+// Explanation Routes
+router.post("/getExplanation", explanationController.get_explanation);
+// Exports
+module.exports = router;
