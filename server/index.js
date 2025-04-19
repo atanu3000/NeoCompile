8559948ae1e44c215161cb5221abdb6d94ded1c6
Atanu Paul <atanupaul2411@gmail.com>
2025-04-19 22:04:42 +0530

    fix: update CORS origin to remove trailing slash

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://neocompile.netlify.app/'],
+  origin: ['http://localhost:5173', 'https://neocompile.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
