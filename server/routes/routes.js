0bd3810017dede349d0a302c788ff792e49fc957
Atanu Paul <atanupaul2411@gmail.com>
2025-04-19 10:42:31 +0530

    fix: correct import path casing for controller module

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = new Router();
 
 // Import Controllers
-import { push_code, explain_code } from "../controllers/controller.js";
+import { push_code, explain_code } from "../Controllers/controller.js";
 
 // Code Routes
 router.post("/runCode", push_code);
